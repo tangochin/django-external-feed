53f4f579301106f382f339c286f0b98bf88f833e
Maurits van Rees <maurits@vanrees.org>
2013-07-17 17:20:13 +0200

    Add templatetags: feeds, single_feed, feed_entry and feed_entry_title.

--- externalfeed/views.py
+++ externalfeed/views.py
@@ -1,17 +1,10 @@
 from django.views.generic.base import TemplateView
 
-from externalfeed.utils import feeds, feeditem
-
 
 class Index(TemplateView):
     """Index of feed items.
     """
     template_name = 'externalfeed/index.html'
-
-    def get_context_data(self, **kwargs):
-        data = super(Index, self).get_context_data(**kwargs)
-        data['feeds'] = feeds()
-        return data
 
 
 class List(Index):
@@ -34,12 +27,6 @@ class Entry(TemplateView):
             # Split the path into a key (to know which feed source to
             # query) and the rest of the path.
             key, path = path.split('/', 1)
-            entry = feeditem(key, path)
-        else:
-            entry = None
-        # We could show a 404 error here, if wanted:
-        # if entry is None:
-        #     from django.http import Http404
-        #     raise Http404
-        data['entry'] = entry
+            data['key'] = key
+            data['path'] = path
         return data
