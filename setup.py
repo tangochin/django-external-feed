e50154dd5778540dc894de8093c9b1fd4deb2a0f
Maurits van Rees <maurits@vanrees.org>
2013-07-17 12:04:53 +0200

    Make this a python package.

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+version = '0.1.dev0'
+
+setup(name='django-external-feed',
+      version=version,
+      description="Show content from an XML feed on your own site.",
+      long_description=open('README.md').read(),
+      # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=['Development Status :: 3 - Alpha',
+                   'Environment :: Web Environment',
+                   'Framework :: Django',
+                   'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   ],
+      keywords='rss atom syndication',
+      author='Maurits van Rees',
+      author_email='m.van.rees@zestsoftware.nl',
+      url='https://github.com/nederhoed/django-external-feed',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+      ],
+      )
